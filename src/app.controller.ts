edf9f7f2cb98a3fe7329ced39835ac70f3f9ddcf
Neil Kalman <neil.kalman@growthspace.com>
2024-09-23 12:49:14 +0300

    feat(smee): start smee on app start

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SmeeService } from '@kb-config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly smeeService: SmeeService
+  ) {
+    this.smeeService.initializeSmeeClient();
+  }
 
   @Get()
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SmeeService } from '@kb-config';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    SmeeService
+  ],
 })
 export class AppModule {}
