7fdf6411ca66d34d9b465537581e4f8ff26646ad
Neil Kalman <neil.kalman@growthspace.com>
2024-09-23 16:32:36 +0300

    chore(logs): add log with app url on start

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { configService } from '@kb-config';
 
 import { AppModule } from './app.module';
 import { Documentation } from './documentation';
+import { Logger } from '@nestjs/common';
 
 bootstrap();
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(
     AppModule,
     { snapshot: true }
@@ -16,4 +18,10 @@ async function bootstrap() {
   await Documentation.addDocumentation(app);
 
   await app.listen(configService.config.PORT);
+
+  const appUrl = (await app.getUrl()).replace(
+    /\[.*?\]/,
+    'localhost'
+  );
+  logger.log(`Application is running on: ${ appUrl }`);
 }
