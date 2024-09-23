2c3ece1b1974211b5e3fdbdd30d35f798fff3110
Neil Kalman <neil.kalman@growthspace.com>
2024-09-23 12:22:32 +0300

    start using configit for app configuration

---
+++ src/config/achievibit.config.ts
@@ -0,0 +1,35 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { BaseConfig, Configuration, ConfigVariable } from '@kibibit/configit';
+
+@Configuration()
+export class AchievibitConfig extends BaseConfig {
+  @ConfigVariable('Server port')
+  @IsNumber()
+    PORT: number;
+
+  @ConfigVariable()
+  @IsString()
+    JWT_SECRET: string;
+
+  @ConfigVariable()
+  @IsString()
+    GITHUB_CLIENT_ID: string;
+
+  @ConfigVariable()
+  @IsString()
+    GITHUB_CLIENT_SECRET: string;
+
+  @ConfigVariable()
+  @IsString()
+    GITHUB_CALLBACK_URL: string;
+
+  @ConfigVariable()
+  @IsString()
+  @IsOptional()
+    SMEE_WEBHOOK_PROXY_CHANNEL: string;
+
+  @ConfigVariable()
+  @IsString()
+  @IsOptional()
+    SMEE_WEBHOOK_TARGET_URL: string;
+}
---
+++ src/config/config.service.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-process-env */
+import { ConfigService, IConfigServiceOptions } from '@kibibit/configit';
+
+import { AchievibitConfig } from './achievibit.config';
+
+const ENV_DEFAULT = 'development';
+
+const nodeEnv = process.env.NODE_ENV || ENV_DEFAULT;
+
+export class KbConfigService extends ConfigService<AchievibitConfig> {
+  logger: any;
+
+  constructor(passedConfig?: Partial<AchievibitConfig>, options: IConfigServiceOptions = {}) {
+    super(AchievibitConfig, passedConfig, {
+      skipSchema: nodeEnv !== 'development',
+      ...options
+    });
+  }
+}
+
+export const configService = new KbConfigService() as KbConfigService;
