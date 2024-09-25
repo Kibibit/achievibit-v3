13c12d2c2570e6945ab4a8ddda22bef52b7ef635
Neil Kalman <neil.kalman@growthspace.com>
2024-09-25 10:59:30 +0300

    remove leftover auth module providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,8 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from '@kb-users';
 
 import { BitbucketModule } from './bitbucket/bitbucket.module';
-import { BitBucketStrategy } from './bitbucket/bitbucket.strategy';
 import { GithubModule } from './github/github.module';
-import { GitHubStrategy } from './github/github.strategy';
 import { GitlabModule } from './gitlab/gitlab.module';
-import { GitLabStrategy } from './gitlab/gitlab.strategy';
-import { JwtStrategy } from './jwt/jwt.strategy';
 import { AuthController } from './auth.controller';
 
 @Module({
@@ -17,15 +13,6 @@ import { AuthController } from './auth.controller';
     GithubModule,
     GitlabModule,
     BitbucketModule
-  ],
-  providers: [
-    // Strategies
-    JwtStrategy,
-    GitHubStrategy,
-    GitLabStrategy,
-    BitBucketStrategy
-    // Services
-    // AuthService
   ],
   controllers: [ AuthController ]
 })
