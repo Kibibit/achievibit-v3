8d28cb0feff108b49425c8caa77c4d3827804b43
Neil Kalman <neil.kalman@growthspace.com>
2024-09-25 19:20:22 +0300

    create base orgs + prs services

--- src/organizations/organizations.service.ts
+++ src/organizations/organizations.service.ts
@@ -1,4 +1,39 @@
+import { MongoRepository } from 'typeorm';
+
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { CreateOrganization, Organization, PageMetaModel, PageModel, PageOptionsModel } from '@kb-models';
 
 @Injectable()
-export class OrganizationsService {}
+export class OrganizationsService {
+  constructor(
+    @InjectRepository(Organization)
+    private readonly orgsRepository: MongoRepository<Organization>
+  ) {}
+
+  create(repo: CreateOrganization) {
+    return this.orgsRepository.save(repo);
+  }
+
+  async findAll(
+    pageOptions: PageOptionsModel
+  ) {
+    const [ entities, itemCount ] = await this.orgsRepository.findAndCount({
+      // Sorting by createdAt field
+      order: { createdAt: pageOptions.order },
+      skip: pageOptions.skip,
+      take: pageOptions.take
+    });
+
+    const pageMeta = new PageMetaModel({ itemCount, pageOptionsModel: pageOptions });
+
+    return new PageModel(entities, pageMeta);
+  }
+
+  async findById(name: string) {
+    return await this.orgsRepository.findOne({
+      where: { name }
+    });
+  }
+}
--- src/pull-requests/pull-requests.service.ts
+++ src/pull-requests/pull-requests.service.ts
@@ -1,4 +1,39 @@
+import { MongoRepository } from 'typeorm';
+
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { CreatePullRequest, PageMetaModel, PageModel, PageOptionsModel, PullRequest } from '@kb-models';
 
 @Injectable()
-export class PullRequestsService {}
+export class PullRequestsService {
+  constructor(
+    @InjectRepository(PullRequest)
+    private readonly orgsRepository: MongoRepository<PullRequest>
+  ) {}
+
+  create(repo: CreatePullRequest) {
+    return this.orgsRepository.save(repo);
+  }
+
+  async findAll(
+    pageOptions: PageOptionsModel
+  ) {
+    const [ entities, itemCount ] = await this.orgsRepository.findAndCount({
+      // Sorting by createdAt field
+      order: { createdAt: pageOptions.order },
+      skip: pageOptions.skip,
+      take: pageOptions.take
+    });
+
+    const pageMeta = new PageMetaModel({ itemCount, pageOptionsModel: pageOptions });
+
+    return new PageModel(entities, pageMeta);
+  }
+
+  async findById(name: string) {
+    return await this.orgsRepository.findOne({
+      where: { name }
+    });
+  }
+}
