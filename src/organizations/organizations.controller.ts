3c68821054903b5388d006863d3562143edcec1e
Neil Kalman <neil.kalman@growthspace.com>
2024-09-26 03:07:45 +0300

    add getAll and getOne for orgs and prs

--- src/organizations/organizations.controller.ts
+++ src/organizations/organizations.controller.ts
@@ -1,6 +1,36 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+import { PageOptionsModel } from '@kb-models';
+
+import { OrganizationsService } from './organizations.service';
 
 @Controller('orgs')
 @ApiTags('Organizations')
-export class OrganizationsController {}
+export class OrganizationsController {
+  constructor(
+    private readonly organizationsService: OrganizationsService
+  ) {}
+
+  @Get()
+  @ApiOperation({
+    summary: 'Get all repositories',
+    description: 'Returns a paginated list of all pull requests'
+  })
+  async getOrganizations(
+    @Query() pageOptions: PageOptionsModel
+  ) {
+    return await this.organizationsService.findAll(pageOptions);
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Get repository by id',
+    description: 'Returns a pull requests by its id'
+  })
+  async getOrganization(
+    @Query('id') id: string
+  ) {
+    return await this.organizationsService.findById(id);
+  }
+}
--- src/pull-requests/pull-requests.controller.ts
+++ src/pull-requests/pull-requests.controller.ts
@@ -1,6 +1,41 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+
+import { JwtAuthGuard } from '@kb-guards';
+import { PageOptionsModel } from '@kb-models';
+
+import { PullRequestsService } from './pull-requests.service';
 
 @Controller('prs')
 @ApiTags('Pull Requests')
-export class PullRequestsController {}
+export class PullRequestsController {
+  constructor(
+    private readonly pullRequestsService: PullRequestsService
+  ) {}
+
+  @Get()
+  @ApiOperation({
+    summary: 'Get all repositories',
+    description: 'Returns a paginated list of all pull requests'
+  })
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  async getPullRequests(
+    @Query() pageOptions: PageOptionsModel
+  ) {
+    return await this.pullRequestsService.findAll(pageOptions);
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Get repository by id',
+    description: 'Returns a pull requests by its id'
+  })
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  async getPullRequest(
+    @Query('id') id: string
+  ) {
+    return await this.pullRequestsService.findById(id);
+  }
+}
