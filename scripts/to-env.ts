68540b264c342a87b05fa428223d135c03d62486
Neil Kalman <neil.kalman@growthspace.com>
2024-10-10 23:52:56 +0300

    add script to convert app config to env file

---
+++ scripts/to-env.ts
@@ -0,0 +1,28 @@
+import { omit } from 'lodash';
+import { parse } from 'dotenv';
+import { readJsonSync, writeFileSync } from 'fs-extra';
+
+class EnvService {
+  fromJson(object: Record<string, unknown>) {
+    let envFile = '';
+    for (const key of Object.keys(object)) {
+      envFile += `${ key }="${ object[key] }"\n`;
+    }
+    return envFile;
+  }
+
+  toJson(envFile: string) {
+    return parse(envFile);
+  }
+}
+
+(async () => {
+  const envService = new EnvService();
+
+  const NODE_ENV = process.env.NODE_ENV || 'development';
+  const jsonEnv = readJsonSync(`./.env.${ NODE_ENV }.achievibit.json`);
+  const envFile = envService.fromJson(omit(jsonEnv, '$schema'));
+
+  writeFileSync('./../.env', envFile);
+})();
+
