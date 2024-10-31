2bb2028c95a6620a5366f78136a56ef6f7ccaa74
Neil Kalman <neil.kalman@growthspace.com>
2024-10-31 23:15:36 +0000

    make sure unit tests are working

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -2,6 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SmeeService } from '@kb-config';
+
+jest.mock('fs-extra', () => ({
+  readJSON: jest.fn().mockResolvedValue({
+    name: 'test',
+    description: 'test',
+    version: 'test',
+    license: 'test',
+    repository: 'test',
+  })
+}));
 
 describe('AppController', () => {
   let appController: AppController;
@@ -9,15 +20,18 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [ AppController ],
-      providers: [ AppService ]
+      providers: [
+        AppService,
+        SmeeService
+      ]
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!"', async () => {
+      expect(appController.getApiDetails()).resolves.toMatchSnapshot();
     });
   });
 });
