8edb61005031a346d796fe374f7e4311db2f250e
Neil Kalman <neil.kalman@growthspace.com>
2025-03-19 16:40:29 +0000

    refactor(client): remove unused HttpClient and simplify AppService

--- client/src/app/app.service.ts
+++ client/src/app/app.service.ts
@@ -1,17 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AppService {
-  user: any;
-
-  constructor(
-    private http: HttpClient
-  ) { }
-
-  getLoggedInUser() {
-    return this.http.get('/api/me');
-  }
-}
+export class AppService {}
