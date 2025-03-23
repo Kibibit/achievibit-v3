789f4c3a0a2798124174e817e399f37165967ec6
Neil Kalman <neil.kalman@growthspace.com>
2025-03-23 02:48:34 +0000

    feat(server): add user achievements room handling and test event functionality

--- server/src/events/events.gateway.ts
+++ server/src/events/events.gateway.ts
@@ -1,6 +1,8 @@
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Achievement } from '@kb-models';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @WebSocketGateway()
 export class EventsGateway {
@@ -20,5 +22,31 @@ export class EventsGateway {
   async sendPingMessage() {
     console.log('Sending ping message to all clients...');
     this.server.emit('ping', 'dev check');
+  }
+
+  @SubscribeMessage('join-user-achievements')
+  handleJoinAchievementsRoom(
+    @MessageBody() userId: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    const room = `achievement:${ userId }`;
+    socket.join(room);
+    console.log(`Socket ${ socket.id } joined room: ${ room }`);
+  }
+
+  sendAchievementToUser(username: string, achievement: Partial<Achievement>) {
+    this.server.to(`achievement:${ username }`).emit('new-achievement', achievement);
+  }
+
+  // TODO: Remove this method when done testing
+  @Cron(CronExpression.EVERY_MINUTE)
+  async testAchievementEvent() {
+    console.log('Sending test achievement event');
+    this.sendAchievementToUser('thatkookooguy', {
+      id: 'test-achievement',
+      avatar: 'https://github.com/kibibit.png',
+      name: 'Test Achievement',
+      description: 'This is a test achievement',
+    });
   }
 }
