578a2f17a2336be7d862056da759b3bb93d0db09
Neil Kalman <neil.kalman@growthspace.com>
2025-03-23 03:16:03 +0000

    chore(pnpm): update pnpm-lock

--- client/src/app/services/api/api-error-handler.ts
+++ client/src/app/services/api/api-error-handler.ts
@@ -1,7 +1,42 @@
 import { AxiosError } from 'axios';
-import { ValidationError } from 'class-validator';
 import { StatusCodes } from 'http-status-codes';
 import { createSnackbar } from '@snackbar/core';
+
+/**
+ * Validation error description.
+ */
+export class ValidationError {
+  /**
+   * Object that was validated.
+   *
+   * OPTIONAL - configurable via the ValidatorOptions.validationError.target option
+   */
+  target?: object;
+  /**
+   * Object's property that haven't pass validation.
+   */
+  property: string;
+  /**
+   * Value that haven't pass a validation.
+   *
+   * OPTIONAL - configurable via the ValidatorOptions.validationError.value option
+   */
+  value?: any;
+  /**
+   * Constraints that failed validation with error messages.
+   */
+  constraints?: {
+      [type: string]: string;
+  };
+  /**
+   * Contains all nested validation errors of the property.
+   */
+  children?: ValidationError[];
+  contexts?: {
+      [type: string]: any;
+  };
+}
+
 
 export class ApiErrorHandler {
   private static handleUnauthorized() {
