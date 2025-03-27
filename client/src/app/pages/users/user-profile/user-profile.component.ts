50407722ca99918479c7a7defa76d83d015c48fb
Neil Kalman <neil.kalman@growthspace.com>
2025-03-27 16:09:03 +0000

    update users pages

--- client/src/app/pages/users/user-profile/user-profile.component.ts
+++ client/src/app/pages/users/user-profile/user-profile.component.ts
@@ -1,24 +1,43 @@
 import { AsyncPipe, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 import { User } from '@kibibit/achievibit-sdk';
+
+import { SocketService } from '../../../services/socket.service';
+import { AchievementOneLoveComponent } from '../../../shared/achievement-one-love/achievement-one-love.component';
 
 
 @Component({
   selector: 'kb-user-profile',
   standalone: true,
-  imports: [ NgIf, AsyncPipe ],
+  imports: [ NgIf, AsyncPipe, AchievementOneLoveComponent, RouterLink ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnDestroy {
   private readonly dataKey = 'user';
+  private readonly handleNewAchievement = this.onNewAchievement.bind(this);
   user!: User;
+  achievementNotification: any;
 
   constructor(
-    private readonly route: ActivatedRoute
+    private readonly route: ActivatedRoute,
+    private readonly socketService: SocketService
   ) {
     this.user = this.route.snapshot.data[this.dataKey];
+
+    this.socketService.joinAchievementRoom(this.user.username);
+    this.socketService.onAchievement(this.user.username, this.handleNewAchievement);
+  }
+
+  ngOnDestroy() {
+    this.socketService.offAchievement(this.user.username, this.handleNewAchievement);
+    this.socketService.leaveAchievementRoom(this.user.username);
+  }
+
+  private onNewAchievement(achievement: any) {
+    console.log(`🎖️ New achievement for ${ this.user.username }:`, achievement);
+    this.achievementNotification = achievement;
   }
 }
--- client/src/app/pages/users/users-all/users-all.component.ts
+++ client/src/app/pages/users/users-all/users-all.component.ts
@@ -1,10 +1,12 @@
-import { debounceTime, distinctUntilChanged, shareReplay, startWith, switchMap, tap } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, map, shareReplay, startWith, switchMap, tap } from 'rxjs';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
+import { ServerValidationError } from '../../../services/api/api-error-handler';
 import { UsersApiService } from '../../../services/api/users.service';
+import { FirstErrorPipe } from '../../../shared/pipes/first-error.pipe';
 
 const threeSeconds = 1500;
 const halfASecond = 500;
@@ -12,7 +14,7 @@ const halfASecond = 500;
 @Component({
   selector: 'kb-users-all',
   standalone: true,
-  imports: [ NgFor, AsyncPipe, NgIf, ReactiveFormsModule, RouterLink ],
+  imports: [ NgFor, AsyncPipe, NgIf, ReactiveFormsModule, RouterLink, FirstErrorPipe ],
   templateUrl: './users-all.component.html',
   styleUrl: './users-all.component.scss'
 })
@@ -21,6 +23,10 @@ export class UsersAllComponent {
   private readonly urlUpdateDebounceTime = threeSeconds;
 
   searchControl = new FormControl('');
+  searchControlErrors$ = this.searchControl.statusChanges.pipe(
+    startWith(this.searchControl.status),
+    map(() => this.searchControl.errors || {})
+  );
 
   // Shared stream of the search input
   private search$ = this.searchControl.valueChanges.pipe(
@@ -50,6 +56,19 @@ export class UsersAllComponent {
         query: query || '',
         take: 50
       })
+        .pipe(
+          catchError((error) => {
+            if (error instanceof ServerValidationError) {
+              const queryError = error.errors.find((e) => e.property === 'query');
+              if (queryError) {
+                this.searchControl.setErrors({ server: queryError.constraints });
+                this.searchControl.markAsTouched();
+              }
+            }
+
+            return [];
+          })
+        )
     )
   );
 
