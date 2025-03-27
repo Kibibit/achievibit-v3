3f62b0fb9f8e078e1c2f19929faaa57541c14cc0
Neil Kalman <neil.kalman@growthspace.com>
2025-03-27 16:06:54 +0000

    implement homepage scene

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+
+import { GeneralApiService } from '../../services/api/general.service';
 
 @Component({
   selector: 'kb-home',
@@ -7,6 +9,54 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit, OnDestroy {
+  constructor(
+    private readonly generalApiService: GeneralApiService
+  ) {
+  }
+  // Define the original widths of the images
+  private imageSizes = {
+    'far-clouds': 128,
+    'near-clouds': 144,
+    'far-mountains': 160,
+    'mountains': 320,
+    'trees': 240
+  };
 
+  private readonly originalHeight = 240;
+
+  @ViewChild('parallaxContainer', { static: true })
+    parallaxRef!: ElementRef;
+
+  private resizeListener = () => this.calculateScrollRatios();
+
+  ngAfterViewInit(): void {
+    window.addEventListener('load', this.resizeListener);
+    window.addEventListener('resize', this.resizeListener);
+    this.calculateScrollRatios();
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('load', this.resizeListener);
+    window.removeEventListener('resize', this.resizeListener);
+  }
+
+  // Function to calculate the ratio and set CSS custom properties
+  private calculateScrollRatios(): void {
+    const parallax = this.parallaxRef.nativeElement as HTMLElement;
+    const containerHeight = parallax.offsetHeight;
+
+    const layers = parallax.querySelectorAll('.layer');
+
+    layers.forEach((layer: Element) => {
+      const classList = Array.from(layer.classList);
+      const layerClass = classList.find((cls) => this.imageSizes[cls as keyof typeof this.imageSizes]);
+
+      if (layerClass) {
+        const ratio = containerHeight / this.originalHeight;
+        const actualImageWidth = this.imageSizes[layerClass as keyof typeof this.imageSizes] * ratio;
+        (layer as HTMLElement).style.setProperty('--scroll-width', `${ Math.floor(actualImageWidth) }px`);
+      }
+    });
+  }
 }
