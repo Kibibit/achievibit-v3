9795e415547982974c502f63a9498a7bd71f958c
Neil Kalman <neil.kalman@growthspace.com>
2025-03-27 16:08:04 +0000

    update organizations pages

--- client/src/app/pages/organizations/organization-profile/organization-profile.component.ts
+++ client/src/app/pages/organizations/organization-profile/organization-profile.component.ts
@@ -1,23 +1,42 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 import { Organization } from '@kibibit/achievibit-sdk';
+
+import { SocketService } from '../../../services/socket.service';
+import { AchievementOneLoveComponent } from '../../../shared/achievement-one-love/achievement-one-love.component';
 
 @Component({
   selector: 'kb-organization-profile',
   standalone: true,
-  imports: [ NgIf ],
+  imports: [ NgIf, AchievementOneLoveComponent, RouterLink ],
   templateUrl: './organization-profile.component.html',
   styleUrl: './organization-profile.component.scss'
 })
-export class OrganizationProfileComponent {
+export class OrganizationProfileComponent implements OnDestroy {
   private readonly dataKey = 'organization';
+  private readonly handleNewAchievement = this.onNewAchievement.bind(this);
   org!: Organization;
+  achievementNotification: any;
 
   constructor(
-    private readonly route: ActivatedRoute
+    private readonly route: ActivatedRoute,
+    private readonly socketService: SocketService
   ) {
     this.org = this.route.snapshot.data[this.dataKey];
+
+    this.socketService.joinOrganizationAchievementRoom(this.org.name);
+    this.socketService.onAchievement(this.org.name, this.handleNewAchievement);
+  }
+
+  ngOnDestroy() {
+    this.socketService.offAchievement(this.org.name, this.handleNewAchievement);
+    this.socketService.leaveOrganizationAchievementRoom(this.org.name);
+  }
+
+  private onNewAchievement(achievement: any) {
+    console.log(`🎖️ New achievement for the ${ this.org.name } organization:`, achievement);
+    this.achievementNotification = achievement;
   }
 }
--- client/src/app/pages/organizations/organizations-all/organizations-all.component.ts
+++ client/src/app/pages/organizations/organizations-all/organizations-all.component.ts
@@ -1,10 +1,12 @@
-import { debounceTime, distinctUntilChanged, shareReplay, startWith, switchMap, tap } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, map, shareReplay, startWith, switchMap, tap } from 'rxjs';
 import { AsyncPipe, DatePipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
+import { ServerValidationError } from '../../../services/api/api-error-handler';
 import { OrganizationsApiService } from '../../../services/api/organizations.service';
+import { FirstErrorPipe } from '../../../shared/pipes/first-error.pipe';
 
 const threeSeconds = 1500;
 const halfASecond = 500;
@@ -12,7 +14,7 @@ const halfASecond = 500;
 @Component({
   selector: 'kb-organizations-all',
   standalone: true,
-  imports: [ DatePipe, NgFor, NgIf, AsyncPipe, ReactiveFormsModule, RouterLink ],
+  imports: [ DatePipe, NgFor, NgIf, AsyncPipe, ReactiveFormsModule, RouterLink, FirstErrorPipe ],
   templateUrl: './organizations-all.component.html',
   styleUrl: './organizations-all.component.scss'
 })
@@ -21,6 +23,10 @@ export class OrganizationsAllComponent {
   private readonly urlUpdateDebounceTime = threeSeconds;
 
   searchControl = new FormControl('');
+  searchControlErrors$ = this.searchControl.statusChanges.pipe(
+    startWith(this.searchControl.status),
+    map(() => this.searchControl.errors || {})
+  );
 
   // Shared stream of the search input
   private search$ = this.searchControl.valueChanges.pipe(
@@ -51,6 +57,19 @@ export class OrganizationsAllComponent {
         query: query || '',
         take: 50
       })
+        .pipe(
+          catchError((error) => {
+            if (error instanceof ServerValidationError) {
+              const queryError = error.errors.find((e) => e.property === 'query');
+              if (queryError) {
+                this.searchControl.setErrors({ server: queryError.constraints });
+                this.searchControl.markAsTouched();
+              }
+            }
+
+            return [];
+          })
+        )
     )
   );
 
