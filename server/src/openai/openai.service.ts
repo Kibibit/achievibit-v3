bfa0adeafad5efce412cc981860f71fa99ad4783
Neil Kalman <neil.kalman@growthspace.com>
2025-03-30 01:20:15 +0300

    feat: enhance avatar generation with error handling and logging

--- server/src/openai/openai.service.ts
+++ server/src/openai/openai.service.ts
@@ -20,42 +20,49 @@ export class OpenaiService {
   }
 
   async generateAvatar(repoName: string, description: string, languages: string[]) {
-    if (!this.openai) {
-      return;
-    }
+    try {
+      if (!this.openai) {
+        return;
+      }
 
-    const prompt = `
-    A square, pixel art-style avatar for a GitHub project named '${ repoName }'.
+      const prompt = `
+    A square, pixel art-style avatar for a GitHub project named '${repoName}'.
     The avatar will be used as a profile picture for the project on the project profile page,
     and represents the project, not a person. The avatar should be simple and recognizable.
     It should ABSOLUTELY NOT include any text!
-    The project is ${ description } and is built with ${ languages.join(', ') }.
+    The project is ${description} and is built with ${languages.join(', ')}.
     Design the avatar with a dark background suitable for dark mode, and use a pixelated,
     retro-inspired style. Reflect the purpose and tone of the project through
     simple iconography or visual metaphors, such as symbolic shapes, tools, or characters.
     Do not include any text in the avatar.
     `.trim();
 
-    this.logger.verbose('OpenAI prompt:', { prompt });
+      this.logger.verbose('OpenAI prompt:', { prompt });
 
-    const response = await this.openai.images.generate({
-      model: 'dall-e-3',
-      prompt,
-      n: 1,
-      size: '1024x1024',
-      response_format: 'url',
-    });
+      const response = await this.openai.images.generate({
+        model: 'dall-e-3',
+        prompt,
+        n: 1,
+        size: '1024x1024',
+        response_format: 'url',
+      });
 
-    const originalUrl = response.data[0].url;
-    const encodedUrl = Buffer.from(originalUrl).toString('base64url');
+      const originalUrl = response.data[0].url;
+      const encodedUrl = Buffer.from(originalUrl).toString('base64url');
 
-    const resizedUrl = `/unsafe/rs:fit:200:200/${encodedUrl}`;
+      const resizedUrl = `/unsafe/rs:fit:200:200/${encodedUrl}`;
 
-    // resize image to 200x200
-    const result = await this.imgProxyAxiosInstance.get(resizedUrl, {
-      responseType: 'arraybuffer'
-    });
+      // resize image to 200x200
+      const result = await this.imgProxyAxiosInstance.get<Buffer>(resizedUrl, {
+        responseType: 'arraybuffer'
+      });
 
-    return result.data.toString('base64');
+      return result.data.toString('base64');
+    } catch (error) {
+      this.logger.error('Error generating avatar', error);
+
+      // should not fail the request
+      return undefined;
+    }
   }
 }
